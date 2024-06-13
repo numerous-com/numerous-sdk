0d871028fe7b64aee4e4903b3cdd9719ca05310e
moroderNumerous <153640142+moroderNumerous@users.noreply.github.com>
2024-06-13 12:43:50 +0200

    fix: github login (#8)
    * separate domain and audience and authenticator constructor
     * use custom auth0 domain for authenticator

--- cli/auth/settings.go
+++ cli/auth/settings.go
@@ -1,6 +1,7 @@
 package auth
 
 var (
-	auth0Domain   string = "numerous-dev.eu.auth0.com"
+	auth0Domain   string = "dev-auth.numerous.com"
+	auth0Audience string = "https://numerous-dev.eu.auth0.com/api/v2/"
 	auth0ClientID string = "fzPy9It2ERYZUi4AG5U0vmDSKC7oOxHe"
 )
