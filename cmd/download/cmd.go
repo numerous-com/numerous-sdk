5931eff3d8ad21b5a5af5a379498a3abb0676a0a
Jens Feodor Nielsen <jens.nielsen@numerous.com>
2024-08-08 09:40:28 +0200

    improve download command description

--- cmd/download/cmd.go
+++ cmd/download/cmd.go
@@ -12,16 +12,16 @@ import (
 
 var long string = `Download app sources of the specified app.
 
-Downloads the sources of an app into the specified [app directory], or the
-directory named after the app slug.
+Downloads the sources of an app into the specified [app directory]. If
+[app directory] is not specified the the app slug will be used as the folder
+name.
 
-If an app already exists in the [app directory] and it has a default deployment
-specified organization and app slugs are not required. In this case the app
-source code will be downloaded directly on top of the local source, so be
-careful!
+If an app already exists in [app directory], and a default deployment is
+configured in numerous.toml, then that will be used to identify the app to
+download the source from. In this case the app source code will be downloaded
+directly on top of the local source, so be careful!
 
-Prompts are displayed for all downloaded files that would override an existing
-file.
+A confirmation prompt will be shown if file overwrites are a possibility.
 `
 
 var Cmd = &cobra.Command{
