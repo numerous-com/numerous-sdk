db08f39699964ed68d472bf93793f9b7beadf593
lasse-numerous <lasse.thomsen@numerous.com>
2024-10-25 14:49:38 +0200

    fix(sdk): Fix wrong return type

--- python/src/numerous/frameworks/flask.py
+++ python/src/numerous/frameworks/flask.py
@@ -9,11 +9,12 @@ from numerous.local import is_local_mode, local_user
 class FlaskCookieGetter:
     def cookies(self) -> dict[str, str]:
         """Get the cookies associated with the current request."""
-        cookies = request.cookies
+        cookies = {key: str(val) for key, val in request.cookies.items()}
         if is_local_mode():
             # Update the cookies on the flask server
             user_session.set_user_info_cookie(cookies, local_user)
         return cookies
 
 
-session = user_session.Session(cg=FlaskCookieGetter())
+def get_session() -> user_session.Session:
+    return user_session.Session(cg=FlaskCookieGetter())
