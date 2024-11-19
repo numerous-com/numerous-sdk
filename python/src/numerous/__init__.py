d9680e1ecd6972ad229f28c84220197d69e4637a
jfeodor <98314775+jfeodor@users.noreply.github.com>
2024-11-19 15:56:25 +0100

    feat(cli, python-sdk): remove numerous app engine functionality (#52)
    Removes all functionality related to the Numerous App engine, as development and support for it
    has been suspended.

--- python/src/numerous/__init__.py
+++ python/src/numerous/__init__.py
@@ -1,6 +1,5 @@
 """The python SDK for numerous."""
 
-__all__ = ("action", "app", "container", "field", "html", "slider", "collection")
+__all__ = ("collection",)
 
-from .apps import action, app, container, field, html, slider
 from .collection import collection
--- python/src/numerous/_plotly.py
+++
@@ -1,5 +0,0 @@
-import plotly.graph_objects as go
-
-
-def plotly_html(figure: go.Figure) -> str:
-    return figure.to_html(include_plotlyjs="cdn", full_html=False)  # type: ignore[no-any-return]
