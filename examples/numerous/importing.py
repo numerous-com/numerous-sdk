e64f634aef45bd3a8d66dcaec29b0678f0b0bf46
Jens Feodor Nielsen <jens.nielsen@numerous.com>
2024-11-26 12:51:17 +0100

    chore: remove numerous app engine examples

--- examples/numerous/importing.py
+++
@@ -1,1 +0,0 @@
-from parameters import ParameterApp  # noqa: F401
--- examples/numerous/syntax.py
+++
@@ -1,9 +0,0 @@
-# mypy: ignore-errors
-
-from numerous import app
-
-
-@app
-class SyntaxErrorApp:
-    my_syntax_error str  # noqa: E999 ]
-    my_field: str
