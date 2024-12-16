5979e4861b728e35a0dec7f6d7b9249b0a5195bf
Jens Feodor Nielsen <jens.nielsen@numerous.com>
2024-12-16 14:00:33 +0100

    add documentation of the plotting functions

--- cmd/output/plot.go
+++ cmd/output/plot.go
@@ -37,6 +37,8 @@ func NewPlotWithTerm(data timeseries.Timeseries, term terminal) *Plot {
 	}
 }
 
+// Display the plots data with each output line prefixed with `prefix`, and taking up
+// `height` lines.
 func (p *Plot) Display(prefix string, height int) {
 	cols, _, err := p.term.GetSize()
 	if err != nil {
@@ -47,9 +49,47 @@ func (p *Plot) Display(prefix string, height int) {
 		cols = maxPlotLineWidth
 	}
 
+	axes := p.newAxes(cols, height)
+	rasterCols := cols - len(prefix) - max(len(axes.yLabelMin), len(axes.yLabelMax)) - 1
+	normalized := p.data.Normalize(float64(rasterCols-1), float64(height-1))
+	r := newPlotRaster(rasterCols, height)
+
+	var prev *timeseries.NormalizedPoint
+	for _, cur := range normalized {
+		if prev == nil {
+			prev = ref(cur)
+			continue
+		}
+
+		r.BresenhamLine(int(prev.X), int(prev.Y), int(cur.X), int(cur.Y))
+
+		prev = ref(cur)
+	}
+
+	w := p.term.Writer()
+	fmt.Fprint(w, p.render(r, axes, prefix, height))
+}
+
+// Render the plotted raster, and the axes with the given prefix, returning it as a
+// string.
+func (p *Plot) render(raster *plotRaster, axes plotAxes, prefix string, height int) string {
+	s := ""
+	for y := height - 1; y >= 0; y-- {
+		ln := prefix + axes.renderYAxisAtHeight(y)
+		ln += raster.renderRow(y)
+		s += ln + "\n"
+	}
+	s += axes.renderXAxis(prefix) + "\n"
+
+	return s
+}
+
+func (p *Plot) newAxes(width, height int) plotAxes {
 	axes := plotAxes{
 		xLabelMin: p.data.MinTimestamp().Format(time.RFC3339),
 		xLabelMax: p.data.MaxTimestamp().Format(time.RFC3339),
+		width:     width,
+		height:    height,
 	}
 
 	if p.data.MinValue() == p.data.MaxValue() {
@@ -60,76 +100,65 @@ func (p *Plot) Display(prefix string, height int) {
 		axes.yLabelMax = fmt.Sprintf("%.2f", p.data.MaxValue())
 	}
 
-	rasterCols := cols - len(prefix) - max(len(axes.yLabelMin), len(axes.yLabelMax)) - 1
-	normalized := p.data.Normalize(float64(rasterCols-1), float64(height-1))
-	r := newRaster(rasterCols, height)
-
-	var prev *timeseries.NormalizedPoint
-	for _, cur := range normalized {
-		if prev == nil {
-			prev = ref(cur)
-			continue
-		}
-
-		r.bresenhamLine(int(prev.X), int(prev.Y), int(cur.X), int(cur.Y))
-
-		prev = ref(cur)
-	}
-
-	w := p.term.Writer()
-	fmt.Fprint(w, p.render(r, prefix, cols, height, axes))
+	return axes
 }
 
-func (p *Plot) render(raster *plotRaster, prefix string, width int, height int, axes plotAxes) string {
-	s := ""
-	for y := height - 1; y >= 0; y-- {
-		ln := prefix + p.renderLeftAxisAtHeight(y, height, axes)
-		ln += raster.renderRow(y)
-		s += ln + "\n"
-	}
-	s += p.renderHorizontalAxis(axes, prefix, width) + "\n"
-
-	return s
-}
-
-func (p *Plot) renderHorizontalAxis(axes plotAxes, prefix string, width int) string {
-	maxYLabelLen := max(len(axes.yLabelMax), len(axes.yLabelMin))
-
-	hz := strings.Repeat(" ", maxYLabelLen-len(axes.yLabelMin)) + axes.yLabelMin + string(boxUpLeftRight)
-	hzRemaining := width - utf8.RuneCountInString(hz) - len(prefix)
-	if hzRemaining > 0 {
-		hz += strings.Repeat(string(boxHorizontal), hzRemaining)
-	}
-
-	xLabelLn := strings.Repeat(" ", maxYLabelLen)
-	xLabelSpacing := width - len(xLabelLn) - len(axes.xLabelMin) - len(axes.xLabelMax) - len(prefix)
-	if xLabelSpacing > 0 {
-		xLabelLn += axes.xLabelMin
-		xLabelLn += strings.Repeat(" ", xLabelSpacing)
-		xLabelLn += axes.xLabelMax
-	}
-
-	return prefix + hz + "\n" + prefix + xLabelLn
-}
-
-func (p *Plot) renderLeftAxisAtHeight(y int, height int, axes plotAxes) string {
-	maxLabelLen := max(len(axes.yLabelMax), len(axes.yLabelMin))
-
-	if y == height-1 {
-		return strings.Repeat(" ", maxLabelLen-len(axes.yLabelMax)) + axes.yLabelMax + string(boxDownLeft)
-	}
-
-	return strings.Repeat(" ", maxLabelLen) + string(boxVertical)
-}
-
+// contains the information needed to render the axes of a plot
 type plotAxes struct {
+	height    int
+	width     int
 	yLabelMin string
 	yLabelMax string
 	xLabelMin string
 	xLabelMax string
 }
 
-func newRaster(width, height int) *plotRaster {
+// Renders a single line of the (vertical) y-axis of a plot, and return it as a
+// string.
+// For the top line it will render the y-axis maximum value.
+func (pa *plotAxes) renderYAxisAtHeight(y int) string {
+	maxLabelLen := max(len(pa.yLabelMax), len(pa.yLabelMin))
+
+	if y == pa.height-1 {
+		return strings.Repeat(" ", maxLabelLen-len(pa.yLabelMax)) + pa.yLabelMax + string(boxDownLeft)
+	}
+
+	return strings.Repeat(" ", maxLabelLen) + string(boxVertical)
+}
+
+// Renders the (horizontal) x-axis of a plot and returns it as a string.
+// It renders two lines - one containing the minimum y-value as well as the
+// actual box-drawn line, and another containing the x-axis labels.
+func (pa *plotAxes) renderXAxis(prefix string) string {
+	maxYLabelLen := max(len(pa.yLabelMax), len(pa.yLabelMin))
+
+	hz := strings.Repeat(" ", maxYLabelLen-len(pa.yLabelMin)) + pa.yLabelMin + string(boxUpLeftRight)
+	hzRemaining := pa.width - utf8.RuneCountInString(hz) - len(prefix)
+	if hzRemaining > 0 {
+		hz += strings.Repeat(string(boxHorizontal), hzRemaining)
+	}
+
+	xLabelLn := strings.Repeat(" ", maxYLabelLen)
+	xLabelSpacing := pa.width - len(xLabelLn) - len(pa.xLabelMin) - len(pa.xLabelMax) - len(prefix)
+	if xLabelSpacing > 0 {
+		xLabelLn += pa.xLabelMin
+		xLabelLn += strings.Repeat(" ", xLabelSpacing)
+		xLabelLn += pa.xLabelMax
+	}
+
+	return prefix + hz + "\n" + prefix + xLabelLn
+}
+
+// A raster for plotting graphs onto.
+// Has related methods for drawing lines onto the raster, and for rendering the
+// raster to strings.
+type plotRaster struct {
+	data   []rune
+	width  int
+	height int
+}
+
+func newPlotRaster(width, height int) *plotRaster {
 	return &plotRaster{
 		data:   make([]rune, width*height),
 		width:  width,
@@ -137,12 +166,7 @@ func newRaster(width, height int) *plotRaster {
 	}
 }
 
-type plotRaster struct {
-	data   []rune
-	width  int
-	height int
-}
-
+// Render a single row (or line) of the raster, returning it as a string.
 func (r *plotRaster) renderRow(y int) string {
 	ln := ""
 	for x := 0; x < r.width; x++ {
@@ -157,7 +181,8 @@ func (r *plotRaster) renderRow(y int) string {
 	return ln
 }
 
-// plot a data point, marking the specified cell and all cells below it
+// Plot a data point, marking the specified cell and all cells below it with the
+// specified rune.
 func (r *plotRaster) plot(x, y int, val rune) {
 	if x < 0 || x >= r.width || y < 0 || y >= r.height {
 		return
@@ -166,6 +191,24 @@ func (r *plotRaster) plot(x, y int, val rune) {
 	for yPlot := y; yPlot >= 0; yPlot-- {
 		i := yPlot*r.width + x
 		r.data[i] = val
+	}
+}
+
+// Draw a line onto the raster
+// See: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+func (r *plotRaster) BresenhamLine(x0, y0, x1, y1 int) {
+	if math.Abs(float64(y1-y0)) < math.Abs(float64(x1-x0)) {
+		if x0 > x1 {
+			r.bresenhamLow(x1, y1, x0, y0)
+		} else {
+			r.bresenhamLow(x0, y0, x1, y1)
+		}
+	} else {
+		if y0 > y1 {
+			r.bresenhamHigh(x1, y1, x0, y0)
+		} else {
+			r.bresenhamHigh(x0, y0, x1, y1)
+		}
 	}
 }
 
@@ -209,22 +252,6 @@ func (r *plotRaster) bresenhamHigh(x0, y0, x1, y1 int) {
 			D += (2 * (dx - dy)) // nolint:mnd
 		} else {
 			D += 2 * dx // nolint:mnd
-		}
-	}
-}
-
-func (r *plotRaster) bresenhamLine(x0, y0, x1, y1 int) {
-	if math.Abs(float64(y1-y0)) < math.Abs(float64(x1-x0)) {
-		if x0 > x1 {
-			r.bresenhamLow(x1, y1, x0, y0)
-		} else {
-			r.bresenhamLow(x0, y0, x1, y1)
-		}
-	} else {
-		if y0 > y1 {
-			r.bresenhamHigh(x1, y1, x0, y0)
-		} else {
-			r.bresenhamHigh(x0, y0, x1, y1)
 		}
 	}
 }
