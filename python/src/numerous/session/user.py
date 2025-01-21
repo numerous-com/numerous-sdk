c947d0673d61ffc8f3f6d3794a0de7fffe4d944c
laurenalexander-em <lauren.alexander@energymachines.com>
2025-01-21 13:55:22 +0100

    docs(python-sdk): removed typo

--- python/src/numerous/session/user.py
+++ python/src/numerous/session/user.py
@@ -35,7 +35,7 @@ class User:
         A collection scoped for the current user. It is a child-collection of the
         root collection with the key "users", and has the user's ID as key.
 
-        Equivalent to:l
+        Equivalent to:
         >>> collection("users").collection(user.id)
 
         Returns:
